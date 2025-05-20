f3be180bdd4422fa9c3c59fc222c3ad5a10b80ec
cameronrome <cameronrome@gmail.com>
2025-05-19 19:25:12 -0700

    Created new scene for game mechanics to be tested in. Made scripts for movement and camera following.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    private Vector2 move;
+
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+    Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+    if (movement != Vector3.zero) //optional, makes it so rotation angle stays when not moving
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed);
+    }
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+    
+}
+}
