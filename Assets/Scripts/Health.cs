a452b58a50de8eff0eb9fe664b8f6698f7f77cb1
cameronrome <cameronrome@gmail.com>
2025-07-08 11:58:39 -0700

    added UI for health bar, health script, UI overlay for combat

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private float current_health;
+    [SerializeField] private float max_health;
+    [SerializeField] private Image health_bar;
+    void Start()
+    {
+        current_health = max_health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health_bar.fillAmount = Mathf.Clamp(current_health / max_health, 0, 1);
+    }
+
+    void TakeDamage(float amount)
+    {
+        current_health -= amount;
+        current_health = Mathf.Min(current_health, 0);
+
+        if (current_health <= 0)
+        {
+            health_bar.gameObject.SetActive(false);
+            // actions when die
+        }
+    }
+
+    void Heal(float amount)
+    {
+        current_health += amount;
+        current_health = Mathf.Min(current_health, max_health);
+    }
+}
