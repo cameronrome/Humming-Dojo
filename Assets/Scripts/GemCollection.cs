133c5bfb9ec78b026aa17f537c86e3d6378206ab
Quockerz <cromeis@uci.edu>
2025-07-25 15:46:49 -0700

    combining changes in tutorial block out

--- Assets/Scripts/GemCollection.cs
+++ Assets/Scripts/GemCollection.cs
@@ -1,17 +1,21 @@
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GemCollection : MonoBehaviour
 {
     private int Gems = 0;
+    [SerializeField] private TextMeshProUGUI gem_text;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Gem"))
         {
             Gems++;
-            Debug.Log(Gems);
             Destroy(other.gameObject);
+
+            gem_text.text = "" + Gems;
         }
     }
 }
