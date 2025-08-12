d683b65f7ef721fbb02eb94a5b0c479635642383
Jerry Liu <liujerry7@gmail.com>
2025-08-12 09:23:59 -0700

    Implemented hum dial timer

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class State
+{
+    protected float stateTimer;
+
+    public virtual void Enter()
+    {
+    }
+
+    public virtual void Exit()
+    {
+    }
+
+    public virtual void Update()
+    {
+        stateTimer -= Time.deltaTime;
+    }
+
+    public virtual void FixedUpdate()
+    {
+    }
+}
