88298eee9a1b4b9bd0cb67df7af951c694a1bb14
cameronrome <cameronrome@gmail.com>
2025-10-06 18:08:11 -0700

    added cameras for trigger zones, zooming in and zooming out in the world, while still being controllable

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,31 +1,58 @@
 using UnityEngine;
 using Unity.Cinemachine;
+using System.Xml.Serialization;
 
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] private CinemachineCamera birdCam; //3
     [SerializeField] private CinemachineCamera shoulderCam; //2
-    [SerializeField] private CinemachineCamera zoomInCam; //1
+    [SerializeField] private CinemachineCamera breatheCam; //1
+    [SerializeField] private CinemachineCamera zoomOutCam; //4
+    [SerializeField] private CinemachineCamera zoomInCam; //5
 
     public void SwitchToShoulderCam()
     {
         birdCam.Priority = 3;
-        shoulderCam.Priority = 5;
-        zoomInCam.Priority = 1;
+        shoulderCam.Priority = 10;
+        breatheCam.Priority = 1;
+        zoomOutCam.Priority = 4;
+        zoomInCam.Priority = 5;
     }
 
     public void SwitchToBirdCam()
     {
-        birdCam.Priority = 5;
+        birdCam.Priority = 10;
         shoulderCam.Priority = 2;
-        zoomInCam.Priority = 1;
+        breatheCam.Priority = 1;
+        zoomOutCam.Priority = 4;
+        zoomInCam.Priority = 5;
     }
 
     public void SwitchToZoomInCam()
     {
         birdCam.Priority = 3;
         shoulderCam.Priority = 2;
+        breatheCam.Priority = 10;
+        zoomOutCam.Priority = 4;
         zoomInCam.Priority = 5;
+    }
+
+    public void SwitchtoZoomOutCam()
+    {
+        birdCam.Priority = 3;
+        shoulderCam.Priority = 2;
+        breatheCam.Priority = 1;
+        zoomOutCam.Priority = 10;
+        zoomInCam.Priority = 5;
+    }
+
+    public void SwitchtoSmallCam()
+    {
+        birdCam.Priority = 3;
+        shoulderCam.Priority = 2;
+        breatheCam.Priority = 1;
+        zoomOutCam.Priority = 4;
+        zoomInCam.Priority = 10;
     }
 
 
